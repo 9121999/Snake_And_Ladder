89f5ab90a30dbf87c9c7cbab20d1bac550a07d14
Prerana <dahiwadeprerana@gmail.com>
2022-11-19 17:03:48 +0530

    [prerana]Add:Reach100

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,8 +6,8 @@ namespace SnakeLadderProblem
     {
         static void Main(string[] args)
         {
-            SankeLadder p = new SankeLadder();
-            p.GamePlay();
+            SankeLadder sn = new SankeLadder();
+            sn.GamePlay();
         }
     }
 }
